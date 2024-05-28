66d66fa955f45e1d44fad37a0684d616e481caaf
11 <111>
2024-05-28 22:48:21 +0800

    1

--- table/game/131/table_131_imagePro.lua
+++ table/game/131/table_131_imagePro.lua
@@ -4,17 +4,17 @@ return {
     [1] = {
         ID = 1,
         gailv100 = 9830,
-        gailv150 = 9670,
+        gailv150 = 9650,
         gailv200 = 9800,
-        gailv50 = 9635,
-        gailv75 = 9715,
+        gailv50 = 9625,
+        gailv75 = 9710,
         type = 1,
     },
 
     [2] = {
         ID = 2,
         gailv100 = 85,
-        gailv150 = 330,
+        gailv150 = 350,
         gailv200 = 100,
         gailv50 = 200,
         gailv75 = 235,
@@ -26,8 +26,8 @@ return {
         gailv100 = 85,
         gailv150 = 0,
         gailv200 = 100,
-        gailv50 = 165,
-        gailv75 = 50,
+        gailv50 = 175,
+        gailv75 = 55,
         type = 3,
     },
 
